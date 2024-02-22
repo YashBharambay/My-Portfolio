1d61d4bab1bfffc977b6b78cb235568f26061aec
YashBharambay <ybharambay@gmail.com>
2024-02-22 18:05:26 -0500

    Change the address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
           <div className="col-lg-5 mr-lg-5 col-12">
             <div className="google-map w-100">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3023.1197964125204!2d-74.06565662450036!3d40.73738943606172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2573a9aafe7bd%3A0x722cd0ca507efd84!2s3049%20John%20F.%20Kennedy%20Blvd%2C%20Jersey%20City%2C%20NJ%2007306!5e0!3m2!1sen!2sus!4v1708134555275!5m2!1sen!2sus"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.781902227706!2d-74.02822342358527!3d40.74482433560505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259deeb426a4d%3A0x52b7295ea9b98396!2sStevens%20Institute%20of%20Technology!5e0!3m2!1sen!2sus!4v1708643062929!5m2!1sen!2sus"
                 width="400"
                 height="300"
                 frameBorder="0"
